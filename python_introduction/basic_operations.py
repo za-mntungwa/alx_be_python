f4dffbee3a9d68c3e7dfd8f1a89dcbef4a0be333
Victor Mntungwa <147992270+za-mntungwa@users.noreply.github.com>
2025-06-01 14:39:04 +0200

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# script that performs basic arithmetic operations with two predefined numbers
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print('Addition', 'of', number1, 'and', number2, 'is', sum)
+print('Subtraction', 'of', number1, 'and', number2, 'is', difference)
+print('Multiplication', 'of', number1, 'and', number2, 'is', product)
