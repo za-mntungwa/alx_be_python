34766acbcdd7a54ccae5f3506b225e5ad421e787
Victor Mntungwa <147992270+za-mntungwa@users.noreply.github.com>
2025-06-01 21:01:33 +0200

    Add weather_advice.py

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,13 @@
+# provide clothing advice based on the given weather conditions
+
+weather = input("What is the weather like today? (sunny/rainy/cold): ").lower()
+
+if weather == "sunny":
+    print('Wear a t-shirt and sunglasses.')
+elif weather == "rainy":
+    print('Don\'t forget your umbrella and a raincoat.')
+elif weather == "cold":
+    print('Make sure to wear a warm coat and a scarf.')
+else:
+    print('Sorry, I don\'t have recommendations for this weather.')
+    
