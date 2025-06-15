d0008f596b7dc249b574d5a88dd338eef019a685
Victor Mntungwa <147992270+za-mntungwa@users.noreply.github.com>
2025-06-15 23:29:48 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,5 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        return print(f"Current Balance: $float({self.account_balance})")
+        self.account_balance = float(self.account_balance)
+        return print(f"Current Balance: ${self.account_balance}")
