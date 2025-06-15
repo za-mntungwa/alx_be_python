9012d0c7bc5258c3ee424f3ed420067cbd23a29b
Victor Mntungwa <147992270+za-mntungwa@users.noreply.github.com>
2025-06-15 23:20:10 +0200

    Update main-0.py

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -21,7 +21,7 @@ def main():
         else:
             print("Insufficient funds.")
     elif command == "display":
-        account.display_balance()
+        print(f"Current Balance: ${account.display_balance()}")
     else:
         print("Invalid command.")
 
